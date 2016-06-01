9950b4ef7437aba2d12b6159777e4fa493aea1e1
MaurerThomas <tommie_maurer@hotmail.com>
2016-06-01 19:26:15 +0200

    Fixes

--- src/main/java/hro/ictlab/dashboard/services/StartContainerResource.java
+++ src/main/java/hro/ictlab/dashboard/services/StartContainerResource.java
@@ -1,6 +1,7 @@
 package hro.ictlab.dashboard.services;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
@@ -21,14 +22,15 @@ public class StartContainerResource {
         if (startContainerByIdResponse.getStatusInfo().getFamily() != Response.Status.Family.SUCCESSFUL) {
             return Response.status(Response.Status.SERVICE_UNAVAILABLE).build();
         } else {
-            return Response.ok().entity(requestToStartContainerById(id)).build();
+            return Response.status(Response.Status.CREATED).entity(startContainerByIdResponse.getStatus()).build();
         }
     }
 
     private Response requestToStartContainerById(String id) throws IOException {
         // Get the IP address from the system environment.
-        URL url = new URL(System.getenv("NODEMANAGER") + id + "/start/");
-        //URL url = new URL("http://145.24.222.223:54623/api/Command/" + id + "-start/");
+        //URL url = new URL(System.getenv("NODEMANAGER") + id + "/start/");
+        // Testing URL
+        URL url = new URL("http://145.24.222.223:8080/nodemanager/api/containers/" + id + "/start/");
         return failOver.handleUrl(url);
     }
 }
